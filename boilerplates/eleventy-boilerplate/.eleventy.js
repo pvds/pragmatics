443aeacb98b4e4725225a91e137d07b464f12098
Peter van der Steen <peter.van.der.steen@priva.nl>
2020-09-19 10:45:28 +0300

    feat: add offline mode

--- boilerplates/eleventy-boilerplate/.eleventy.js
+++ boilerplates/eleventy-boilerplate/.eleventy.js
@@ -1,5 +1,6 @@
 const pluginRss = require('@11ty/eleventy-plugin-rss');
 const pluginNavigation = require('@11ty/eleventy-navigation');
+const pluginPWA = require('eleventy-plugin-pwa');
 const markdownIt = require('markdown-it');
 
 const filters = require('./utils/filters.js');
@@ -11,6 +12,10 @@ module.exports = function (config) {
   // Plugins
   config.addPlugin(pluginRss);
   config.addPlugin(pluginNavigation);
+  config.addPlugin(pluginPWA, {
+    swDest: './dist/sw.js',
+    globDirectory: './dist',
+  });
 
   // Filters
   Object.keys(filters).forEach((filterName) => {
